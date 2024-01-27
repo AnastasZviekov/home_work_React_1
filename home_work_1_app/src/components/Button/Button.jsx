7a161413d383f92d349a659d1c6e1312e22ed200
AnastasZviekov <stas.zviekov@gmail.com>
2024-01-27 13:38:51 +0100

    add_components

---
+++ home_work_1_app/src/components/Button/Button.jsx
@@ -0,0 +1,8 @@
+
+import './style.css'
+const Button=()=>{
+return <button>Push me</button>
+}
+
+
+export default Button;
---
+++ home_work_1_app/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import Button from "../Button/Button.jsx";
+import Input from "../Input/Input.jsx";
+const Header=()=>{
+    return (<header>
+        <Button/>
+        <Input/>
+    </header>)
+}
+
+export default Header;
